a79fc8acf3b66ab69bcd3e88d7b4b1f8e9f818e3
Doğan Dinçer DEMİRCİ <47913600+ddincerdem@users.noreply.github.com>
2021-11-02 15:16:52 +0300

    Ben bir şey yapmadım hepsini
    Resul yaptı

--- Bean Wars/Assets/Scripts/Player/Movement.cs
+++ Bean Wars/Assets/Scripts/Player/Movement.cs
@@ -10,25 +10,22 @@ public class Movement : MonoBehaviour
     private bool grounded;
 
 
-    // Start is called before the first frame update
     void Awake()
     {
         rigidBody = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
     void Update()
     {
         float horizantalInput = Input.GetAxis("Horizontal");
         rigidBody.velocity = new Vector2(horizantalInput * speed, rigidBody.velocity.y);
 
-        //horizantalInput is always 0, if click the right button, it should be 1, and -1
-
-        /* if (horizantalInput > 0.01f)
-            transform.localScale = Vector3.one;
-        else if (horizantalInput < -0.01f)
-            transform.localScale = new Vector3(-1, 1, 1); */
-
+        Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        
+        if (mousePos.x >= transform.position.x)       
+             transform.localScale = Vector3.one;
+        else
+            transform.localScale = new Vector3(-1, 1, 1);
 
         if (Input.GetKey(KeyCode.Space) && grounded)
             Jump();
