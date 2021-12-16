ef3d2f1afff241e1c5fb49d891e11e35ddf84f02
Nurettin Resul Tanyıldızı <tanyildiziresul@gmail.com>
2021-12-16 21:03:42 +0300

    changes

--- Dinowars/Assets/Scripts/Lobby/DinowarsNetworkManager.cs
+++ Dinowars/Assets/Scripts/Lobby/DinowarsNetworkManager.cs
@@ -2,6 +2,7 @@ using System;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using Mirror;
+using System.Collections.Generic;
 
 /*
 	Documentation: https://mirror-networking.gitbook.io/docs/components/network-manager
@@ -10,12 +11,16 @@ using Mirror;
 
 public class DinowarsNetworkManager : NetworkManager
 {
+    [SerializeField] private int minPlayers = 2;
     [SerializeField] private string menuscene = string.Empty;
     [Header("Room")]
     [SerializeField] private DinowarsNetworkRoomPlayer roomPlayerPrefab;
 
     public static event Action OnClientConnected;
     public static event Action OnClientDisconnected;
+    public static event Action OnPlayersUpdated;
+
+    public List<DinowarsNetworkRoomPlayer> RoomPlayers { get; } = new List<DinowarsNetworkRoomPlayer>();
 
     public override void OnStartServer()
     {
@@ -24,9 +29,22 @@ public class DinowarsNetworkManager : NetworkManager
 
     public override void OnStartClient()
     {
-        foreach(var prefab in spawnPrefabs)
+        foreach (var prefab in spawnPrefabs)
         {
             NetworkClient.RegisterPrefab(prefab);
+        }
+    }
+
+    public override void OnServerConnect(NetworkConnection conn)
+    {
+        if (numPlayers >= maxConnections)
+        {
+            conn.Disconnect(); return;
+        }
+
+        if (!SceneManager.GetActiveScene().name.Equals(menuscene))
+        {
+            conn.Disconnect(); return;
         }
     }
 
@@ -37,6 +55,16 @@ public class DinowarsNetworkManager : NetworkManager
         Debug.Log("Client is connected: " + conn.connectionId);
     }
 
+    public override void OnServerAddPlayer(NetworkConnection conn)
+    {
+        if (SceneManager.GetActiveScene().name.Equals(menuscene))
+        {
+            DinowarsNetworkRoomPlayer roomPlayerInstance = Instantiate(roomPlayerPrefab);
+            roomPlayerInstance.IsLeader = RoomPlayers.Count == 0;
+            NetworkServer.AddPlayerForConnection(conn, roomPlayerInstance.gameObject);
+        }
+    }
+
     public override void OnClientDisconnect(NetworkConnection conn)
     {
         base.OnClientDisconnect(conn);
@@ -44,26 +72,40 @@ public class DinowarsNetworkManager : NetworkManager
         Debug.Log("Client is disconnected: " + conn.connectionId);
     }
 
-    public override void OnServerConnect(NetworkConnection conn)
+    public override void OnServerDisconnect(NetworkConnection conn)
     {
-        if (numPlayers >= maxConnections)
+        if (conn.identity != null)
         {
-            conn.Disconnect(); return;
-        }
+            DinowarsNetworkRoomPlayer player = conn.identity.GetComponent<DinowarsNetworkRoomPlayer>();
+            RoomPlayers.Remove(player);
 
-        if(!SceneManager.GetActiveScene().name.Equals(menuscene))
-        {
-            conn.Disconnect(); return;
+            // NotifyPlayersReadyState
         }
-
+        base.OnServerDisconnect(conn);
     }
 
-    public override void OnServerAddPlayer(NetworkConnection conn)
+    public override void OnStopServer()
     {
-        if (SceneManager.GetActiveScene().name.Equals(menuscene))
-        {
-            DinowarsNetworkRoomPlayer roomPlayerInstance = Instantiate(roomPlayerPrefab);
-            NetworkServer.AddPlayerForConnection(conn, roomPlayerInstance.gameObject);
+        RoomPlayers.Clear();
+    }
+
+    public void NotifyPlayersReadyState()
+    {
+        foreach(var player in RoomPlayers) {
+            player.HandleReadyToStart(IsReadyToStart());
         }
     }
+
+    public bool IsReadyToStart()
+    {
+        if (numPlayers < minPlayers) return false;
+
+        foreach (var player in RoomPlayers) {
+            if (!player.IsReady) return false;
+        }
+
+        return true;
+    }
+
+
 }
--- Dinowars/Assets/Scripts/Lobby/DinowarsNetworkRoomPlayer.cs
+++ Dinowars/Assets/Scripts/Lobby/DinowarsNetworkRoomPlayer.cs
@@ -5,6 +5,15 @@ using UnityEngine;
 
 public class DinowarsNetworkRoomPlayer : NetworkBehaviour
 {
+    public bool IsLeader { get; set; } = false;
+    public bool IsReady { get; set; } = false;
+
+    public void HandleReadyToStart(bool isReadyToStart)
+    {
+        if (!IsLeader) return;
+
+    }
+
     // Start is called before the first frame update
     void Start()
     {
