a8e120c669b701fc472a110969bdc9c276720be5
Nurettin Resul Tanyıldızı <tanyildiziresul@gmail.com>
2022-01-03 00:53:12 +0300

    deneme

--- Dinowars/Assets/Scripts/Other/Lawa.cs
+++ Dinowars/Assets/Scripts/Other/Lawa.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class Lawa : MonoBehaviour
 {
+    [SerializeField] private double damage = 100;
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.CompareTag("Player"))
